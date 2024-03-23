2a4aaf3d5e76eb9dd1490f746de01d68975427a6
Vedant <vedantpmokati@gmail.com>
2024-03-23 23:36:05 +0530

    app

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -23,10 +23,7 @@ class DataTransformation:
         self.data_transformation_config=DataTransformationConfig()
 
     def get_data_transformer_object(self):
-        '''
-        This function si responsible for data trnasformation
-        
-        '''
+       
         try:
             numerical_columns = ["writing_score", "reading_score"]
             categorical_columns = [
